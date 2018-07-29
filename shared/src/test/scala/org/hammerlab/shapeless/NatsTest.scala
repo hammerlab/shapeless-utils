ff30dc26f6b7c578313c4f86143014834c3e7748
Ryan Williams <ryan.blake.williams@gmail.com>
2018-07-29 19:29:50 +0000

    add implicit Nat instances

---
+++ shared/src/test/scala/org/hammerlab/shapeless/NatsTest.scala
@@ -0,0 +1,32 @@
+package org.hammerlab.shapeless
+
+import hammerlab.shapeless._
+import shapeless.Nat._
+
+class NatsTest
+  extends hammerlab.Suite {
+  test("implicits") {
+    implicitly[_1 ]
+    implicitly[_2 ]
+    implicitly[_3 ]
+    implicitly[_4 ]
+    implicitly[_5 ]
+    implicitly[_6 ]
+    implicitly[_7 ]
+    implicitly[_8 ]
+    implicitly[_9 ]
+    implicitly[_10]
+    implicitly[_11]
+    implicitly[_12]
+    implicitly[_13]
+    implicitly[_14]
+    implicitly[_15]
+    implicitly[_16]
+    implicitly[_17]
+    implicitly[_18]
+    implicitly[_19]
+    implicitly[_20]
+    implicitly[_21]
+    implicitly[_22]
+  }
+}
