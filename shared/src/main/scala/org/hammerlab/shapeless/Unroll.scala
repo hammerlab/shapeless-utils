56c119ba9e7b9eb00bb230c06f6ac7e3c0b966f5
Ryan Williams <ryan.blake.williams@gmail.com>
2018-08-02 22:43:31 +0000

    parameterize Unroll by a type-ctor arg

--- shared/src/main/scala/org/hammerlab/shapeless/Unroll.scala
+++ shared/src/main/scala/org/hammerlab/shapeless/Unroll.scala
@@ -3,27 +3,27 @@ package org.hammerlab.shapeless
 import shapeless._
 import shapeless.nat._
 
-sealed trait Unroll[In] {
+sealed trait Unroll[In, T[_]] {
   type N <: Nat
   type Out
 }
 
 trait LowPriUnroll {
-  type Aux[In, _N <: Nat, _Out] =
-    Unroll[In] {
+  type Aux[In, T[_], _N <: Nat, _Out] =
+    Unroll[In, T] {
       type N = _N
       type Out = _Out
     }
 
-  type Natd[In, _N <: Nat] =
-    Unroll[In] {
+  type Natd[In, T[_], _N <: Nat] =
+    Unroll[In, T] {
       type N = _N
     }
 
-  implicit def zero[T]: Aux[T, _0, T] =
-    new Unroll[T] {
+  implicit def zero[In, T[_]]: Aux[In, T, _0, In] =
+    new Unroll[In, T] {
       type N = _0
-      type Out = T
+      type Out = In
     }
 }
 
@@ -31,18 +31,20 @@ object Unroll
   extends LowPriUnroll {
   implicit def rep[
     In,
+    T[_],
     _N <: Nat,
     _Out
   ](
     implicit
-    prev: Lazy[Aux[In, _N, _Out]]
+    prev: Lazy[Aux[In, T, _N, _Out]]
   ):
     Aux[
-      Seq[In],
+      T[In],
+      T,
       Succ[_N],
       _Out
     ] =
-    new Unroll[Seq[In]] {
+    new Unroll[T[In], T] {
       type N = Succ[_N]
       type Out = _Out
     }
--- shared/src/test/scala/org/hammerlab/shapeless/UnrollTest.scala
+++ shared/src/test/scala/org/hammerlab/shapeless/UnrollTest.scala
@@ -1,18 +1,33 @@
 package org.hammerlab.shapeless
 
 import Unroll.Aux
+import shapeless.Nat
 import shapeless.nat._
 
 class UnrollTest
   extends hammerlab.Suite {
   test("derivations") {
-    implicitly[Unroll[Seq[Int]]]
-    implicitly[Unroll[Seq[Seq[Int]]]]
-    implicitly[Unroll[Seq[Seq[Seq[Int]]]]]
+    // Count the levels of [[Seq]]:
+    implicitly[Unroll[Seq[Int], Seq]]
+    implicitly[Unroll[Seq[Seq[Int]], Seq]]
+    implicitly[Unroll[Seq[Seq[Seq[Int]]], Seq]]
 
-    implicitly[Aux[Int, _0, Int]]
-    implicitly[Aux[Seq[Int], _1, Int]]
-    implicitly[Aux[Seq[Seq[Int]], _2, Int]]
-    implicitly[Aux[Seq[Seq[Seq[Int]]], _3, Int]]
+    implicitly[Aux[Int, Seq, _0, Int]]
+    implicitly[Aux[Seq[Int], Seq, _1, Int]]
+    implicitly[Aux[Seq[Seq[Int]], Seq, _2, Int]]
+    implicitly[Aux[Seq[Seq[Seq[Int]]], Seq, _3, Int]]
+
+    // Same derivations, but with a type-alias fixing [[Seq]] as the type-constructor
+    type U[S] = Unroll[S, Seq]
+    type UAux[S, N <: Nat, Out] = Aux[S, Seq, N, Out]
+
+    implicitly[U[Seq[Int]]]
+    implicitly[U[Seq[Seq[Int]]]]
+    implicitly[U[Seq[Seq[Seq[Int]]]]]
+
+    implicitly[UAux[Int, _0, Int]]
+    implicitly[UAux[Seq[Int], _1, Int]]
+    implicitly[UAux[Seq[Seq[Int]], _2, Int]]
+    implicitly[UAux[Seq[Seq[Seq[Int]]], _3, Int]]
   }
 }
